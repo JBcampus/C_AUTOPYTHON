ccf8dfca12984864c5483268f9825b023ec9cc08
Wilfredo P <wilfredoparedes4@gmail.com>
2026-08-24 22:35:31 -0500

    feat: implementación selenium y primer test con uso de fixture y validaciones en pytest.

--- playground/clase3/fixtures_selenium.py
+++ playground/clase3/fixtures_selenium.py
@@ -1,9 +1,17 @@
 import pytest 
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service 
  
 @pytest.fixture(scope='function') 
 def driver(): 
-    navegador = webdriver.Chrome()
+    # 1. Definimos la ruta exacta al driver que ya descargaste
+    ruta_driver = r"./drivers/msedgedriver.exe"
+
+    # 2. Creamos el servicio con esa ruta
+    servicio = Service(executable_path=ruta_driver)
+
+    # 3. Le pasamos el servicio a webdriver.Edge
+    navegador = webdriver.Edge(service=servicio)
     navegador.maximize_window() 
  
     yield navegador 
