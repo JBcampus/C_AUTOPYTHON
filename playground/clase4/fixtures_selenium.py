b7c1dd0e3a0e2ed9ef5b8550c2360018d48701f5
Flavio M <flaviomontero22@gmail.com>
2026-08-26 22:21:23 -0500

    feat(clase4): implementación de esperas explicitas, clase ycaptura de evidencias.

---
+++ playground/clase4/fixtures_selenium.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope= 'function')
+def driver():
+    navegador = webdriver.Edge()
+    navegador.maximize_window()
+
+    yield navegador
+
+    navegador.quit
