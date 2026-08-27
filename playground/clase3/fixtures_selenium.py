b9d6847add5d85a11c5b12f197e295ac04f4667b
Wilfredo P <wilfredoparedes4@gmail.com>
2026-08-26 22:41:51 -0500

    feat(clase4): implementación de esperas explicitas, clase y captura de evidencias.

--- playground/clase3/fixtures_selenium.py
+++ playground/clase3/fixtures_selenium.py
@@ -1,17 +1,9 @@
-import pytest 
-from selenium import webdriver
-from selenium.webdriver.edge.service import Service 
+import pytest
+from selenium import webdriver 
  
-@pytest.fixture(scope='function') 
+@pytest.fixture 
 def driver(): 
-    # 1. Definimos la ruta exacta al driver que ya descargaste
-    ruta_driver = r"./drivers/msedgedriver.exe"
-
-    # 2. Creamos el servicio con esa ruta
-    servicio = Service(executable_path=ruta_driver)
-
-    # 3. Le pasamos el servicio a webdriver.Edge
-    navegador = webdriver.Edge(service=servicio)
+    navegador = webdriver.Edge() 
     navegador.maximize_window() 
  
     yield navegador 
