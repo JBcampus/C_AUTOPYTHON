f42f143801fad2053b587c52fde55c01712cfdb3
AdelinaB <aabravoc@gmail.com>
2026-08-26 22:16:30 -0500

    feat(clase4):implementacion de esperas explicitas

---
+++ playground/clase4/fixtures_selenium.py
@@ -0,0 +1,12 @@
+
+import pytest
+from selenium import webdriver 
+ 
+@pytest.fixture
+def driver(): 
+    navegador = webdriver.Edge() 
+    navegador.maximize_window() 
+ 
+    yield navegador 
+ 
+    navegador.quit() 
