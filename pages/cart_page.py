290698dfcb4bb8f006dce22a81c0be498cd98603
Wilfredo P <wilfredoparedes4@gmail.com>
2026-08-31 18:08:29 -0500

    feat(tarea2): Desarrollo de suite Selenium, implementación estabilidad y evidencias

---
+++ pages/cart_page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+class CartPage(BasePage):
+    """Mapea las acciones dentro del carrito de compras de la aplicación."""
+
+    PROCEED_TO_CHECKOUT_BTN = (By.CSS_SELECTOR, ".check_out")
+
+    def is_url_correct(self) -> bool:
+        """Espera a que la URL del navegador contenga '/view_cart'.
+
+        Returns:
+            bool: True si la URL de navegación es la correcta.
+        """
+        return self.wait_url_contains("/view_cart")
+
+    def proceed_to_checkout_and_screenshot(self, image_name="evidencia_final.png"):
+        """Hace clic en 'Proceed to Checkout' y toma la captura requerida por el taller.
+
+        Args:
+            image_name (str): Nombre del archivo de imagen para la evidencia.
+        """
+        self.click(self.PROCEED_TO_CHECKOUT_BTN)
+        self.take_screenshot(image_name)
---
+++ tests/tarea2/test_purchase.py
@@ -0,0 +1,37 @@
+from playground.clase4.fixtures_selenium import driver
+from pages.home_page import HomePage
+from pages.products_page import ProductsPage
+from pages.cart_page import CartPage
+
+def test_suite_seleccion_y_compra(driver):
+    """Test 1: Flujo completo analizando estabilidad de carga."""
+    home = HomePage(driver)
+    products = ProductsPage(driver)
+    cart = CartPage(driver)
+
+    print("\n[PASO 1] Navegando a la plataforma...")
+    home.navigate()
+
+    print("[PASO 2] Validando sección features_items...")
+    assert home.is_features_items_displayed(), "La sección de ítems destacados no cargó."
+
+    print("[PASO 3] Haciendo clic en la marca POLO...")
+    home.click_polo_brand()
+
+    print("[PASO 4] Verificando cambio de URL de la marca...")
+    assert products.is_url_correct(), "La URL no se actualizó con la palabra 'Polo'."
+
+    print("[PASO 5] Agregando el primer ítem al carrito...")
+    products.add_first_item_to_cart()
+
+    print("[PASO 6] Esperando visibilidad del Pop-up emergente...")
+    assert products.is_popup_visible(), "El pop-up de confirmación no se mostró."
+
+    print("[PASO 7] Accediendo a la vista del carrito...")
+    products.click_view_cart()
+
+    print("[PASO 8] Validando URL interna del carrito...")
+    assert cart.is_url_correct(), "No se redirigió correctamente al carrito."
+
+    print("[PASO 9] Confirmando el Checkout y guardando evidencia...")
+    cart.proceed_to_checkout_and_screenshot("evidencia_checkout.png")
