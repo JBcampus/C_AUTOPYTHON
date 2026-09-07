ad5c28576a28c03485bb9f62bb2f900adb53e16b
Flavio M <flaviomontero22@gmail.com>
2026-09-07 10:15:45 -0500

    feat(clase5): implementación de Page Object Model con Selenium.

---
+++ tests/clase5/test_login_pom.py
@@ -0,0 +1,7 @@
+import pytest
+
+@pytest.mark.smoke
+def test_login_correcto_con_pom(login_page, inventory_page):
+    login_page.abrir()
+    login_page.iniciar_sesion("standard_user", "secret_sauce")
+    assert inventory_page.obtener_titulo() == "Products"
