adababf7c2bd30a0b1161ea450d162ce77fba601
Yury M <isyurymonzonv@gmail.com>
2026-09-07 22:43:27 -0500

    feat: CI GitHub.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,49 +1,17 @@
-import pytest 
-from selenium import webdriver 
- 
-from pages.clase5.login_page import LoginPage 
-from pages.clase5.inventory_page import InventoryPage 
-from pages.clase5.alerts_page import AlertsPage 
-from pages.clase5.upload_page import UploadPage 
- 
-@pytest.fixture 
-def driver(): 
-    navegador = webdriver.Edge() 
-    navegador.maximize_window() 
- 
-    yield navegador 
- 
-    navegador.quit() 
- 
-@pytest.fixture(scope="class") 
-def driver_class(): 
-    navegador = webdriver.Edge() 
-    navegador.maximize_window() 
- 
-    yield navegador 
- 
-    navegador.quit() 
- 
-@pytest.fixture 
-def login_page(driver): 
-    return LoginPage(driver) 
- 
-@pytest.fixture 
-def inventory_page(driver): 
-    return InventoryPage(driver) 
- 
-@pytest.fixture(scope="class") 
-def login_page_class(driver_class): 
-    return LoginPage(driver_class) 
- 
-@pytest.fixture(scope="class") 
-def inventory_page_class(driver_class): 
-    return InventoryPage(driver_class) 
- 
-@pytest.fixture 
-def alerts_page(driver): 
-    return AlertsPage(driver) 
- 
-@pytest.fixture 
-def upload_page(driver): 
-    return UploadPage(driver)
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+#Navegación headless sin interfaz grafica
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    navegador = webdriver.Chrome(options=options)
+
+    yield navegador
+    
+    navegador.quit()
